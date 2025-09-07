5b53a8be754a56683be28ada0f347226b3eac0ae
esosedov <eg.sosedov@gmail.com>
2025-09-07 18:26:47 +0300

    add log all exception

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -3,16 +3,31 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const errorResponse = exception.getResponse();
+
+    this.logger.error({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      body: JSON.stringify(request.body ?? {}),
+      params: request.params,
+      query: request.query,
+      response: errorResponse,
+    });
 
     response.status(status).json({
       statusCode: status,
